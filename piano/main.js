8fbb089a157100377544de9865f17fe14180d6ca
Jay Lietzow <lietzowja@alliancecityschools.org>
2017-11-19 20:38:28 +0000

    created piano project

---
+++ piano/main.js
@@ -0,0 +1,66 @@
+function playC() {
+    var noteC = new Audio("media/028.wav");
+    noteC.play();
+}
+function playCS() {
+    var noteCS = new Audio("media/029.wav");
+    noteCS.play();
+}
+function playD() {
+    var noteD = new Audio("media/030.wav");
+    noteD.play();
+}
+function playEF() {
+    var noteEF = new Audio("media/031.wav");
+    noteEF.play();
+}
+function playE() {
+    var noteE = new Audio("media/032.wav");
+    noteE.play();
+}
+function playF() {
+    var noteF = new Audio("media/033.wav");
+    noteF.play();
+}
+function playFS() {
+    var noteFS = new Audio("media/034.wav");
+    noteFS.play();
+}
+function playG() {
+    var noteG = new Audio("media/035.wav");
+    noteG.play();
+}
+function playAF() {
+    var noteAF = new Audio("media/036.wav");
+    noteAF.play();
+}
+function playA() {
+    var noteA = new Audio("media/037.wav");
+    noteA.play();
+}
+function playBF() {
+    var noteBF = new Audio("media/038.wav");
+    noteBF.play();
+}
+function playB() {
+    var noteB = new Audio("media/039.wav");
+    noteB.play();
+}
+function playC2() {
+    var noteC2 = new Audio("media/040.wav");
+    noteC2.play();
+}
+    
+$("#cKey").on("click", playC);
+$("#cSKey").on("click", playCS);
+$("#dKey").on("click", playD);
+$("#eFKey").on("click", playEF);
+$("#eKey").on("click", playE);
+$("#fKey").on("click", playF);
+$("#fSKey").on("click", playFS);
+$("#gKey").on("click", playG);
+$("#aFKey").on("click", playAF);
+$("#aKey").on("click", playA);
+$("#bFKey").on("click", playBF);
+$("#bKey").on("click", playB);
+$("#c2Key").on("click", playC2);
